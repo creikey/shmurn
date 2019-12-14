c265d2d92d3335f8636a12a61994d89d02277fcf
Cameron Reikes <cameronreikes@gmail.com>
2019-12-13 23:15:20 -0800

    Make zombies go to the center of the screen

--- src/ZombieSpawner.gd
+++ src/ZombieSpawner.gd
@@ -10,7 +10,7 @@ func _process(delta):
 	if counter >= 0.05:
 		var cur_zombie: Node2D = preload("res://Zombie.tscn").instance()
 		add_child(cur_zombie)
-		var max_size: float = max(get_viewport().size.x, get_viewport().size.y)
+		var max_size: float = 1000
 		cur_zombie.global_position = Vector2(max_size*2.0, 0).rotated(rand_range(0, 2*PI))
-		cur_zombie.rotation = cur_zombie.global_position.angle_to_point(get_viewport().size/2)+PI
+		cur_zombie.rotation = cur_zombie.global_position.angle_to_point(get_global_mouse_position())+PI
 		counter = 0.0
